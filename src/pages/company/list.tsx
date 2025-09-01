3693e318934e0b2fe35d236844d5c06a1068f444
RJ Umair <umairajput711@gmail.com>
2025-09-01 13:39:21 +0500

    changes done

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -9,12 +9,13 @@ import { Input, Space, Table, Typography } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { COMPANIES_LIST_QUERY } from '@/components/account-settings/queries';
 import CustomAvatar from '@/components/custom-avatar';
-import { Company } from '@/graphql/schema.types';
 import { currencyNumber } from '@/utilities';
 import { useState } from 'react';
-import { GetFieldsFromList } from '@refinedev/nestjs-query';
 import { HttpError } from "@refinedev/core";
-import { CompaniesListQuery } from '@/graphql/types';
+import { CompaniesListQuery } from "@/graphql/types";
+
+// Table row type: one node from the GraphQL query
+type CompanyRow = CompaniesListQuery['companies']['nodes'][number];
 
 const CompanyList = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
@@ -27,25 +28,15 @@ const CompanyList = ({ children }: React.PropsWithChildren) => {
     setCurrent,
     setPageSize,
     setFilters,
-  } = useTable<GetFieldsFromList<CompaniesListQuery>, HttpError, GetFieldsFromList<CompaniesListQuery>>({
+  } = useTable<CompanyRow, HttpError>({
     resource: 'companies',
-    pagination: {
-      pageSize: 12,
-    },
-    sorters: {
-      initial: [
-        {
-          field: 'createdAt',
-          order: 'desc',
-        },
-      ],
-    },
-    meta: {
-      gqlQuery: COMPANIES_LIST_QUERY,
-    },
+    pagination: { pageSize: 12 },
+    sorters: { initial: [{ field: 'createdAt', order: 'desc' }] },
+    meta: { gqlQuery: COMPANIES_LIST_QUERY },
   });
 
-  const companies = data?.data ?? [];
+  // data?.data is already the array of nodes
+  const companies: CompanyRow[] = data?.data ?? [];
 
   const handleSearch = (value: string) => {
     setSearchText(value);
@@ -66,13 +57,8 @@ const CompanyList = ({ children }: React.PropsWithChildren) => {
           <CreateButton
             onClick={() => {
               go({
-                to: {
-                  resource: 'companies',
-                  action: 'create',
-                },
-                options: {
-                  keepQuery: true,
-                },
+                to: { resource: 'companies', action: 'create' },
+                options: { keepQuery: true },
                 type: 'replace',
               });
             }}
@@ -90,7 +76,7 @@ const CompanyList = ({ children }: React.PropsWithChildren) => {
           />
         </div>
 
-        <Table<Company>
+        <Table<CompanyRow>
           dataSource={companies}
           rowKey="id"
           loading={isLoading}
@@ -100,43 +86,44 @@ const CompanyList = ({ children }: React.PropsWithChildren) => {
             onChange: setCurrent,
             onShowSizeChange: (_, size) => setPageSize(size),
           }}
-          columns={[
-            {
-              dataIndex: "name",
-              title: "Company Title",
-              render: (_: unknown, record: Company) => (
-                <Space>
-                  <CustomAvatar shape="square" name={record.name} src={record.avatarUrl} />
-                  <Typography.Text style={{ whiteSpace: "nowrap" }}>
-                    {record.name}
-                  </Typography.Text>
-                </Space>
-              ),
-            },
-            {
-              dataIndex: "totalRevenue",
-              title: "Open deals amount",
-              render: (_: unknown, company: Company) => (
-                <Typography.Text>
-                  {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+        >
+          <Table.Column<CompanyRow>
+            dataIndex="name"
+            title="Company Title"
+            render={(_, record) => (
+              <Space>
+                <CustomAvatar shape="square" name={record.name} src={record.avatarUrl} />
+                <Typography.Text style={{ whiteSpace: 'nowrap' }}>
+                  {record.name}
                 </Typography.Text>
-              ),
-            },
-            {
-              dataIndex: "id",
-              title: "Actions",
-              fixed: "right",
-              render: (value: string) => (
-                <Space>
-                  <EditButton hideText size="small" recordItemId={value} />
-                  <DeleteButton hideText size="small" recordItemId={value} />
-                </Space>
-              ),
-            },
-          ]}
-        />
+              </Space>
+            )}
+          />
 
+          <Table.Column<CompanyRow>
+            dataIndex="totalRevenue"
+            title="Open deals amount"
+            render={(_, record) => (
+              <Typography.Text>
+                {currencyNumber(record.dealsAggregate?.[0]?.sum?.value || 0)}
+              </Typography.Text>
+            )}
+          />
+
+          <Table.Column<CompanyRow>
+            dataIndex="id"
+            title="Actions"
+            fixed="right"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
       </List>
+
       {children}
     </div>
   );
